00cad5bf16ab6e09dd23fc98cb11441e88e09de5
tiennq <bechanhtt010@gmail.com>
2023-03-23 17:26:29 +0700

    nhucc

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-
-
-
-int main(int, char**) {
-    int n,k;
-    std::cin >>n >> k;
-    std::cout << n << " " << k;
-    std::cout << "Hello, world!\n";
+#include<iostream>
+int main(){
+    printf("abcdefghijklmnopxyzvw");
 }
