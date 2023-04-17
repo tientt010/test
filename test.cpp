f8dfe9f264b617485c7cb980bd06d3788ae607e7
tieu cot <tieucottt010@gmail.com>
2023-04-18 02:18:56 +0700

    test

--- test.cpp
+++ test.cpp
@@ -1,27 +1,6 @@
-#include<bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
-
-string lineEncoding(string s)
-{
-    string p;
-    int n=s.size();
-    int k=1;
-    for(int i=0;i<n-1;i++){
-        if(s[i+1]==s[i]){
-            k++;
-        }else{
-            if(k==1)p=p+s[i];
-            else p=p+(char)(k+48)+s[i];
-            k=1;
-        }
-    }
-    if(k!=1)p=p+(char)(k+48)+s[s.size()-2];
-    else p=p+s[s.size()];
-    return p;
-}
-
 int main(){
-    string s;
-    cin>>s;
-    cout<<lineEncoding(s);
+    cout<<"tach clb roi";
 }
