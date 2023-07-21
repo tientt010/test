8bd2fcd2012f73f363b09e32d045f9f3298cb6fe
tientt010 <tieucottt010@gmail.com>
2023-07-21 22:01:56 +0700

    test

--- test.cpp
+++ test.cpp
@@ -1,44 +1,7 @@
-//1. Cột có tổng lớn nhất. Đưa ra giá trị lớn nhất đó
-//Input:
-//3 4
-//8 5 2 3
-//9 2 12 10
-//1 9 10 2
-//Output:
-//24
+#include<iostream>
+#include<vector>
 
-//2. Tìm phần tử là số nguyên tố lớn nhất trong ma trận 
-//Input:   
-//3 3
-//1 5 3 
-//9 4 7
-//10 2 6
-//Output:
-//7
-
-//3. Tìm UCLN, BCNN của 2 số
-//Input:
-//12 15
-//Output:
-//3 60
-
-//4. Tìm số bé nhất và bé thứ 2 trong mảng 1 chiều
-//Input:
-//5
-//7 4 2 9 1
-//Output:
-//1 2
-
-//5. Cộng 2 ma trận
-//Input:
-//3 3
-//1 2 3
-//2 2 2 
-//6 5 4
-//2 2 1
-//1 1 1
-//1 0 0
-//Output:
-//3 4 4
-//3 3 3
-//7 5 4
+using namespace std;
+int main(){
+    vector<int> v;
+}
